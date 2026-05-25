94d35bcd110d91a40ab0a9eda56d4bc6eb868d02
Junghyun Seo <jseo0508@gmail.com>
2026-05-25 18:47:55 +0900

    fix: RefreshToken 엔티티 추가 및 Provider Enum 적용

---
+++ src/main/java/com/example/teblyserver/auth/repository/RefreshTokenRepository.java
@@ -0,0 +1,10 @@
+package com.example.teblyserver.auth.repository;
+
+import com.example.teblyserver.auth.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+}
