5b1e39fe9e18ae44cdc6197ea6e5e2045f9dc0df
Junghyun Seo <jseo0508@gmail.com>
2026-05-25 10:27:19 +0900

    fix: JWT SecretKey 환경변수로 분리

--- src/main/java/com/example/teblyserver/auth/service/JwtService.java
+++ src/main/java/com/example/teblyserver/auth/service/JwtService.java
@@ -3,17 +3,23 @@ package com.example.teblyserver.auth.service;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Service
 public class JwtService {
 
-    private final SecretKey key = Keys.hmacShaKeyFor(new byte[32]);
+    private final SecretKey key;
     private final long ACCESS_TOKEN_EXPIRE = 1000 * 60 * 60; // 1시간
     private final long REFRESH_TOKEN_EXPIRE = 1000 * 60 * 60 * 24 * 14; // 2주
+
+    public JwtService(@Value("${jwt.secret}") String secretString) {
+        this.key = Keys.hmacShaKeyFor(secretString.getBytes(StandardCharsets.UTF_8));
+    }
 
     public String generateAccessToken(Long userId) {
         return Jwts.builder()
